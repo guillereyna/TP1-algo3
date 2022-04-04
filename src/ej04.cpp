1a90a77b572d8be94a629ba5560daae191638203
greyna <guile.j.reyna@gmail.com>
2022-04-04 19:54:53 -0300

    fixes y cambiar uso de iterador de vector a uso de una variable global para asegurar complejidad

--- src/ej04.cpp
+++ src/ej04.cpp
@@ -1,5 +1,7 @@
 #include "ej04.h"
 
+int i = 0;
+
 vector<actividad> actividades;
 int cantActividades;
 
@@ -7,13 +9,15 @@ bool terminaAntes(actividad a, actividad b){
     return a.t <= b.t;
 }
 
+/* encuentra la actividad que termina lo más temprano posible y no se solapa
+   con el último horario ocupado y devuelve su horario de fin, o -1 si no lo 
+   encuentra */
 int elegirGoloso(vector<actividad> &actividades, int h){
-    int i = 0;
     int res = -1;
     while(i < actividades.size() && res == -1){
         if (actividades[i].s < h){
             res = actividades[i].t;
-            actividades.erase(actividades.begin()+i);
+            //actividades.erase(actividades.begin(), actividades.begin()+1+i);
         }
         i++;
     }
@@ -21,20 +25,21 @@ int elegirGoloso(vector<actividad> &actividades, int h){
     return res;
 }
 
+/* calcula el beneficio máximo del conjunto de actividades dado que está 
+   ocupado hasta el horario h */
 int actividadesGolosoCalcular (vector<actividad> &actividades, int h){
     int electa = elegirGoloso(actividades, h);
-    if (actividades.size() == 0 || electa == -1){
+    if (i >= actividades.size() || electa == -1){
         return 0;
     } else {
-        return 1 + actividadesGolosoCalcular(actividades, h);
+        return 1 + actividadesGolosoCalcular(actividades, electa);
     }
 }
 
+/* devuelve el beneficio máximo posible del conjunto de actividades pasado 
+   por parámetro */
 int actividadesGoloso (vector<actividad> actividades){
+    i = 0;
     sort(actividades.begin(), actividades.end(), terminaAntes);
     return actividadesGolosoCalcular(actividades, 0);
-}
-
-int main (int argc, char* argv[]){
-    return 0;
 }
--- src/ej04.h
+++ src/ej04.h
@@ -8,6 +8,4 @@ struct actividad {
     int s, t;
 };
 
-int elegirGoloso(vector<actividad> &actividades, int h);
-int actividadesGolosoCalcular (vector<actividad> &actividades);
 int actividadesGoloso (vector<actividad> actividades);
