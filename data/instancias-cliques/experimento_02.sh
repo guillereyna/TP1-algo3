13d46e7db15c27aa50669d7bc6a40318175d89d6
greyna <guille.j.reyna@gmail.com>
2022-04-07 01:09:17 -0300

    directorios para data de cada ejercicio, bash script para cada experimento

---
+++ data/instancias-cliques/experimento_02.sh
@@ -0,0 +1,6 @@
+#bin/usr/bash
+for i in *.clq
+    do
+        echo "Procesando $i"
+        /usr/bin/time -f "%e" timeout 6000 ../../src/backtracking01 < $i > ../output-cliques-02/$i.output
+    done
--- data/instancias-ej04/experimento_04.sh
+++ data/instancias-ej04/experimento_04.sh
@@ -2,5 +2,5 @@
 for i in interval_instance_*
     do
         echo "Procesando $i"
-        /usr/bin/time -f "%e" timeout 6000 ../../src/ej04 < $i > output-$i
+        /usr/bin/time -f "%e" timeout 6000 ../../src/ej04 < $i > ../output-ej04/$i.output
     done
