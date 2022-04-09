10c4455a9b3a29981e173bee728aaa25bde3b6e8
greyna <guille.j.reyna@gmail.com>
2022-04-09 01:57:10 -0300

    comentarios! deberia poder leerse asi numas

--- src/ej04.cpp
+++ src/ej04.cpp
@@ -13,15 +13,20 @@ typedef vector<vector<actividad>> Schedule;
 
 vector<int> S; //subconjunto de actividades solucion
 
+/* construye un vector de 2n vectores de actividades, donde cada elemento i del
+vector contiene un vector con todas las actividades que terminan en la hora i.
+Suponiendo que generar un vector vacio es O(1) y push_back() es O(1), se
+genera un vector en O(2n) y luego se recorre el input en O(n).
+O(2n) + O(n) = O(n) */
 Schedule read_input(){
     cin >> cantActividades;
 
-    Schedule A(2*cantActividades + 1);
+    Schedule A(2*cantActividades + 1); //O(2n)
 
     int start;
     int end;
 
-    for (int i = 0; i < cantActividades; i++){
+    for (int i = 0; i < cantActividades; i++){ //O(n)
         cin >> start >> end;
         actividad act;
         act.pos = i; act.start = start;
@@ -31,19 +36,15 @@ Schedule read_input(){
     return A;
 }
 
-
 /* encuentra la actividad que termina lo más temprano posible y no se solapa
    con el último horario ocupado y devuelve su horario de fin, o -1 si no lo 
    encuentra */
 int elegirGoloso(const Schedule &actividades, int h){
-    //cout << "en goloso h = " << h << endl;
     for (int i = h+1; i < 2*cantActividades+1; i++){
         for(actividad act : actividades[i]){
-            //cout << actividades[i][j].pos << ", " << actividades[i][j].start << " < " << h << endl;
             if (act.start > h){
                 S.push_back(act.pos);
                 return i;
-                //cout << "pusheo el " << i << endl;
             }
         }
     }
@@ -51,12 +52,14 @@ int elegirGoloso(const Schedule &actividades, int h){
 }
 
 /* calcula el beneficio máximo del conjunto de actividades dado que está 
-   ocupado hasta el horario h */
+   ocupado hasta el horario h
+   Siempre se encuentra la proxima actividad que termina lo mas temprano
+   posible arrancando desde el tiempo en el que termina la última actividad
+   elegida golosamente, por lo que se "visita" cada actividad a lo sumo
+   una única vez, en O(n) tiempo. */
 int actividadesGoloso (const Schedule &actividades, int h){
     int electa = elegirGoloso(actividades, h);
-    //cout << "llame la funcion con h = " << h << " y me dio " << electa << endl; 
     if (electa == -1 || electa > 2*cantActividades) return 0;
-    //cout << "llamado recursivo con " << electa << endl;
     return 1 + actividadesGoloso(actividades, electa);
 }
 
