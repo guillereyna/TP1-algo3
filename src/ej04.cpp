050ccc71f96e685b38a5a97dc4196921b7acd418
greyna <guille.j.reyna@gmail.com>
2022-04-09 01:41:38 -0300

    correcto y (creo) que en O(n)

--- src/ej04.cpp
+++ src/ej04.cpp
@@ -16,7 +16,7 @@ vector<int> S; //subconjunto de actividades solucion
 Schedule read_input(){
     cin >> cantActividades;
 
-    Schedule A(2*cantActividades);
+    Schedule A(2*cantActividades + 1);
 
     int start;
     int end;
@@ -35,14 +35,15 @@ Schedule read_input(){
 /* encuentra la actividad que termina lo más temprano posible y no se solapa
    con el último horario ocupado y devuelve su horario de fin, o -1 si no lo 
    encuentra */
-int elegirGoloso(Schedule &actividades, int h){
-    int i = h;
-    for (; i < 2*cantActividades; i++){
-        i++;
+int elegirGoloso(const Schedule &actividades, int h){
+    //cout << "en goloso h = " << h << endl;
+    for (int i = h+1; i < 2*cantActividades+1; i++){
         for(actividad act : actividades[i]){
+            //cout << actividades[i][j].pos << ", " << actividades[i][j].start << " < " << h << endl;
             if (act.start > h){
                 S.push_back(act.pos);
                 return i;
+                //cout << "pusheo el " << i << endl;
             }
         }
     }
@@ -51,15 +52,17 @@ int elegirGoloso(Schedule &actividades, int h){
 
 /* calcula el beneficio máximo del conjunto de actividades dado que está 
    ocupado hasta el horario h */
-int actividadesGoloso (Schedule &actividades, int h){
+int actividadesGoloso (const Schedule &actividades, int h){
     int electa = elegirGoloso(actividades, h);
-    if (electa == -1 || electa >= 2*cantActividades) return 0;
+    //cout << "llame la funcion con h = " << h << " y me dio " << electa << endl; 
+    if (electa == -1 || electa > 2*cantActividades) return 0;
+    //cout << "llamado recursivo con " << electa << endl;
     return 1 + actividadesGoloso(actividades, electa);
 }
 
 int main (){
     Schedule A = read_input();
-    int beneficio = actividadesGoloso(A, 0);
+    int beneficio = actividadesGoloso(A, -1);
     cout << beneficio << endl;
     for (size_t i = 0; i < S.size(); i++){
         cout << S[i] << " ";
