9e2a12af5695e6b0307fe0d1532cb298554cb300
greyna <guile.j.reyna@gmail.com>
2022-04-10 16:19:52 -0300

    ej03 top down de chuma

--- data/instancias-ej03/experimento_03.sh
+++ data/instancias-ej03/experimento_03.sh
@@ -3,5 +3,5 @@ mkdir -p ../output-ej03
 for i in instancia_*
     do
         echo "Procesando $i"
-        /usr/bin/time -f "%e" timeout 6000 ../../src/ej03 < $i > ../output-ej03/$i.output
+        /usr/bin/time -f "%e" timeout 6000 ../../src/ej03_td < $i > ../output-ej03/$i.output
     done
