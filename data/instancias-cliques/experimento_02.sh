71b97735616cda05036ab32459ca30bfcc52a986
greyna <guille.j.reyna@gmail.com>
2022-04-19 18:43:06 -0300

    limpieza de primavera

--- data/instancias-cliques/experimento_02.sh
+++ data/instancias-cliques/experimento_02.sh
@@ -3,5 +3,5 @@ mkdir -p ../output-cliques-02
 for i in *.clq
     do
         echo "Procesando $i"
-        /usr/bin/time -f "%e" timeout 6000 ../../src/backtracking01 < $i > ../output-cliques-02/$i.output
+        /usr/bin/time -f "%e" timeout 600 ../../src/backtracking_02 < $i > ../output-cliques-02/$i.output
     done
--- data/instancias-ej03/experimento_03.sh
+++ data/instancias-ej03/experimento_03.sh
@@ -4,7 +4,7 @@ mkdir -p ../output-ej03-td
 for i in instancia_*
     do
         echo "Procesando $i"
-        /usr/bin/time -f "%e" timeout 6000 ../../src/ej03_td < $i > ../output-ej03-td/$i\_td.output
+        /usr/bin/time -f "%e" timeout 600 ../../src/ej03_td < $i > ../output-ej03-td/$i\_td.output
     done
 
 echo "Corriendo experimento con implementación bottom-up"
@@ -12,5 +12,5 @@ mkdir -p ../output-ej03-bu
 for i in instancia_*
     do
         echo "Procesando $i"
-        /usr/bin/time -f "%e" timeout 6000 ../../src/ej03_bu < $i > ../output-ej03-bu/$i\_bu.output
+        /usr/bin/time -f "%e" timeout 600 ../../src/ej03_bu < $i > ../output-ej03-bu/$i\_bu.output
     done
